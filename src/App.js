e6f90549649a3222e64f463643083a4c366b1cce
Andrey <ScootalooRainbow@yandex.ru>
2021-03-16 11:56:21 +0900

    cursor done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Footer from "./components/Footer";
 import { gsap, TweenMax, TweenLite, Expo, Power1 } from "gsap";
 
 function App() {
-
   let gradient = {
       value:
         "linear-gradient(180deg, rgba(254,176,42,1) 0%, rgba(253,150,34,1) 100%)",
@@ -41,25 +40,24 @@ function App() {
       gradient:
         "linear-gradient(180deg, rgba(165, 75, 75) 0%, rgba(145, 152, 229) 100%)",
       duration: 5,
-      repeat: 3,
+      repeat: 9999,
       yoyo: true,
     });
-    gsap.set('.follower',{xPercent:-50,yPercent:-50});
-    gsap.set('.cursor',{xPercent:-50,yPercent:-50});
+    gsap.set(".follower", { xPercent: -50, yPercent: -50 });
+    gsap.set(".cursor", { xPercent: -50, yPercent: -50 });
 
-    let follow = document.querySelector('.follower');
-    let cur = document.querySelector('.cursor');
+    let follow = document.querySelector(".follower");
+    let cur = document.querySelector(".cursor");
 
-    window.addEventListener('mousemove',e => {
-        gsap.to(cur,0.2,{x:e.clientX,y:e.clientY});
-        gsap.to(follow,0.9,{x:e.clientX,y:e.clientY});
+    window.addEventListener("mousemove", (e) => {
+      gsap.to(cur, 0.2, { x: e.clientX, y: e.clientY });
+      gsap.to(follow, 0.9, { x: e.clientX, y: e.clientY });
     });
-
   });
   return (
     <body className="body body__gradient">
-      <div  className="cursor"></div>
-      <div  className="follower"></div>
+      <div className="cursor"></div>
+      <div className="follower"></div>
       <div className="page">
         <Main />
         <ContentBox />
