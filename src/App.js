62dc7e31e93f40ac9be93af0002e6a275f892b94
Andrey <ScootalooRainbow@yandex.ru>
2021-04-03 03:24:07 +0900

    this is second commit

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
 
   }, 250);
 //  Hello, my name is Andrey.
+// Здесь будет исправление! Все ок. 
   let gradient = {
       value:
         "linear-gradient(180deg, rgba(254,176,42,1)0%, rgba(253,150,34,1) 100%)",
